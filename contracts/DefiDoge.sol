4552120f543911bc27d22657f12bd0175fa843d9
danush <danushkaadikari@gmail.com>
2021-05-29 23:10:46 +0700

    Changes the pancakerouter to the new one

--- contracts/DefiDoge.sol
+++ contracts/DefiDoge.sol
@@ -753,7 +753,7 @@ contract DefiDoge is Context, IERC20, Ownable {
         _rOwned[_msgSender()] = _rTotal;
         
         // 0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F router for mainnet
-        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0xD99D1c33F9fC3444f8101754aBC46c52416550D1);
+        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x10ED43C718714eb63d5aA57B78B54704E256024E);
          // Create a uniswap pair for this new token
         uniswapV2Pair = IUniswapV2Factory(_uniswapV2Router.factory())
             .createPair(address(this), _uniswapV2Router.WETH());
